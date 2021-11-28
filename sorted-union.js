778f04c6b93497caaa0882307985bb27abf460ce
josephlx2014 <46848008+josephlx2014@users.noreply.github.com>
2021-11-27 21:20:37 -0600

    possible solution

---
+++ sorted-union.js
@@ -0,0 +1,29 @@
+function uniteUnique(arr) {
+
+    let joinedArr = []
+
+    for (let x = 0; x < arguments.length; x++) {
+
+        joinedArr.push(...arguments[x]);
+
+    }
+
+    let newArr = [];
+
+    for (let x = 0; x < joinedArr.length; x++) {
+
+        if (newArr.indexOf(joinedArr[x]) === -1) {
+
+
+            newArr.push(joinedArr[x])
+
+
+        }
+    }
+
+    console.log(newArr);
+
+    return arr;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
