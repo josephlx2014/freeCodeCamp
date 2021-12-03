5de89060ecd07929c847fdb11a80ea6b1c3ecd45
josephlx2014 <46848008+josephlx2014@users.noreply.github.com>
2021-12-02 18:17:10 -0600

    dont know...

--- sorted-union.js
+++ sorted-union.js
@@ -1,29 +1,27 @@
 function uniteUnique(arr) {
 
-    let joinedArr = []
-
-    for (let x = 0; x < arguments.length; x++) {
-
-        joinedArr.push(...arguments[x]);
-
+    let joinedArr=[]
+  
+    for(let x=0; x < arguments.length; x++){
+  
+      joinedArr.push(...arguments[x]);
+    
     }
-
+  
     let newArr = [];
-
-    for (let x = 0; x < joinedArr.length; x++) {
-
-        if (newArr.indexOf(joinedArr[x]) === -1) {
-
-
-            newArr.push(joinedArr[x])
-
-
-        }
+  
+  
+  
+    for(let x=0; x < newArr.length; x++){
+  
+  
     }
-
-    console.log(newArr);
-
-    return newArr;
-}
-
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  
+  
+  
+        console.log(joinedArr);
+  
+    return arr;
+  }
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
