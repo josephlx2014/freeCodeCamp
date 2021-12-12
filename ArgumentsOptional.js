aea115458f95052ad6a2bc7b7b2d395d9e7df049
josephlx2014 <46848008+josephlx2014@users.noreply.github.com>
2021-12-11 18:23:38 -0600

    Solved challenge

--- ArgumentsOptional.js
+++ ArgumentsOptional.js
@@ -1,23 +1,41 @@
 function addTogether(value1, value2) {
-  let numOfArgs = [...arguments];
-  let argumentsCount = numOfArgs.length;
+    let numOfArgs = [...arguments];
+    let argumentsCount = numOfArgs.length;
 
-  if (argumentsCount > 1) {
-    if (isNaN(value1) || isNaN(value2)) {
-      return undefined;
-    } else {
-      return value1 + value2;
+    if (argumentsCount > 1) {
+
+        if (typeof(value1) != "number" || typeof(value2) != "number") {
+            return undefined;
+        } else {
+            return value1 + value2;
+        }
+    } else if (argumentsCount === 1) {
+        if (typeof(value1) != "number") {
+
+            return undefined;
+
+        } else {
+
+            return function(param) {
+                if (typeof(param) === "number") { return value1 + param } else {
+                    return undefined;
+                }
+            };
+
+        }
     }
-  } else if (argumentsCount === 1) {
-    if (isNaN(value1)) {
-      return undefined;
-    } else {
-      return "function";
-    }
-  }
 }
 
-console.log(addTogether(1, 2));
-console.log(addTogether(1));
-console.log(addTogether("helo", "asfd"));
-console.log(addTogether("helo"));
+//var sumThreeAnd = addTogether(3);
+//console.log(sumThreeAnd(2));
+
+// running tests
+//addTogether(2, "3"); //should return undefined.
+console.log(addTogether(2)([3])); //should return undefined.
+console.log(addTogether(2)(3))
+    // tests completed
+
+//console.log(addTogether(1, 2));
+//console.log(addTogether(1));
+//console.log(addTogether("helo", "asfd"));
+//console.log(addTogether("helo"));
